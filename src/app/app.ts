f85a36363146ae7f862c72ab545793240f18917b
Matthijs <bomaltennishal@outlook.com>
2026-01-31 13:20:44 +0100

    prop

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductList } from './product-list/product-list';
+
+let product = ' none'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Oreally_Angular');
 }
-
-
-// https://angular.dev/guide/http/making-requests
