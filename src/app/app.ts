8243b84183c176412c6dcb0ffa75c43f056649b2
AVMSTEVE <mstevens@svb.nl>
2026-02-27 08:38:55 +0100

    2schwarz

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,21 +2,24 @@ import { Component, signal } from '@angular/core';
 import { Header } from "./header/header";
 import { User } from "./user/user";
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   users = DUMMY_USERS;
-<<<<<<< HEAD
+
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id = this.selectedUserId)!
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user: ' + id)
+    this.selectedUserId = id;
   }
-=======
-  hallo = "Hi";
->>>>>>> fac4f2333050f29398e53a1ba4387e55eff95aef
 }
