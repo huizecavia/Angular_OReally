26837c696c7c14d5eaa1fff61adae10cbddcaa82
AVMSTEVE <mstevens@svb.nl>
2026-03-03 11:05:29 +0100

    2schwarz

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,24 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+interface User {
+    id: string;
+    avatar: string;
+    name: string;
+  }
+
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
+
 export class UserComponent {
   // @Input({ required: true }) id!: string;
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
 
-  @Input({required:true}) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  }
+  @Input({required:true}) user!: User;
 
   @Output() select = new EventEmitter();
 
