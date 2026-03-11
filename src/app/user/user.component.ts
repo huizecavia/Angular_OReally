8a27f5b36dc87a861a522eb9d888a47b6d2bf0d4
AVMSTEVE <mstevens@svb.nl>
2026-03-11 16:37:08 +0100

    2schwarz

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { User } from './user.model';
+import { Card } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [Card],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
